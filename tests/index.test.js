b52d61c91690698b543c4502634cc712a5b4e011
Frencis <frencis.balla@gmail.com>
2024-05-21 11:08:46 +0200

    Added tests for use case

--- tests/index.test.js
+++ tests/index.test.js
@@ -1,1 +1,93 @@
-// tests/index.js
+const Scoreboard = require('../src/index');
+
+describe('Scoreboard', () => {
+    let scoreboard;
+
+    beforeEach(() => {
+        scoreboard = new Scoreboard();
+    });
+
+    test('should start a new match', () => {
+        scoreboard.startMatch('Italy', 'Germany');
+
+        expect(scoreboard.getSummary()).toEqual([
+            { homeTeam: 'Italy', awayTeam: 'Germany', homeScore: 0, awayScore: 0 }
+        ]);
+    });
+
+    test('should throw an error if team names are not strings', () => {
+        expect(() => {
+            scoreboard.startMatch(0, 1);
+        }).toThrow('Team names should be strings');
+    });
+
+    test('should update the score of an ongoing match', () => {
+        matchId = scoreboard.startMatch('Italy', 'Germany');
+        scoreboard.updateScore(matchId, 3, 1);
+
+        expect(scoreboard.getSummary()).toEqual([
+            { homeTeam: 'Italy', awayTeam: 'Germany', homeScore: 3, awayScore: 1 }
+        ]);
+    });
+
+    test('should throw an error if non absolute scores are provided', () => {
+        matchId = scoreboard.startMatch('Portugal', 'Norway');
+
+        expect(() => {
+            scoreboard.updateScore(matchId, -1, 2);
+        }).toThrow('Scores must be absolute values');
+
+        expect(() => {
+            scoreboard.updateScore(matchId, 1, -2);
+        }).toThrow('Scores must be absolute values');
+
+        expect(() => {
+            scoreboard.updateScore(matchId, 1.5, 2);
+        }).toThrow('Scores must be absolute values');
+
+        expect(() => {
+            scoreboard.updateScore(matchId, 1, 2.5);
+        }).toThrow('Scores must be absolute values');
+    });
+
+    test('should finish an ongoing match', () => {
+        matchId = scoreboard.startMatch('Spain', 'Croatia');
+        scoreboard.finishMatch(matchId);
+
+        expect(scoreboard.getSummary()).toEqual([]);
+    });
+
+    test('should throw an error if match not found', () => {
+        matchId = "random-match-id"
+
+        expect(() => {
+            scoreboard.updateScore(matchId, -1, 2);
+        }).toThrow('Match not found');
+    });
+
+    test('should get a summary of matches in progress ordered by total score', () => {
+        matchId = scoreboard.startMatch('Mexico', 'Canada');
+        scoreboard.updateScore(matchId, 0, 5);
+
+        matchId = scoreboard.startMatch('Spain', 'Brazil');
+        scoreboard.updateScore(matchId, 10, 2);
+
+        matchId = scoreboard.startMatch('Germany', 'France');
+        scoreboard.updateScore(matchId, 2, 2);
+
+        matchId = scoreboard.startMatch('Uruguay', 'Italy');
+        scoreboard.updateScore(matchId, 6, 6);
+
+        matchId = scoreboard.startMatch('Argentina', 'Australia');
+        scoreboard.updateScore(matchId, 3, 1);
+
+
+        expect(scoreboard.getSummary()).toEqual([
+            { homeTeam: 'Uruguay', awayTeam: 'Italy', homeScore: 6, awayScore: 6 },
+            { homeTeam: 'Spain', awayTeam: 'Brazil', homeScore: 10, awayScore: 2 },
+            { homeTeam: 'Mexico', awayTeam: 'Canada', homeScore: 0, awayScore: 5 },
+            { homeTeam: 'Argentina', awayTeam: 'Australia', homeScore: 3, awayScore: 1 },
+            { homeTeam: 'Germany', awayTeam: 'France', homeScore: 2, awayScore: 2 },
+        ]);
+    });
+});
